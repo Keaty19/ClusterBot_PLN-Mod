32964fdce9ca5f928348ccd7eee4fab35860a29f
Keaty19 <49294230+Keaty19@users.noreply.github.com>
2021-01-25 13:39:23 +0000

    Delete plnTabs.R

--- plnTabs.R
+++
@@ -1,8 +0,0 @@
-Tab1 <- tabPanel("First Tab",
-                 selectInput("select",
-                             "Choose one",
-                             choices = letters[1:3],
-                             selected = 'a'))
-
-Tab2 <- tabPanel("Second Tab",
-                 textOutput('mychoice'))
